fd884b1c3bc389757a99d3cb17db67e658a1ba09
steven daddario <daddarios@dupage.edu>
2021-09-25 05:29:47 -0500

    small change

---
+++ seed.js
@@ -0,0 +1,20 @@
+const db = require('./db');
+
+
+async function seedIt(){
+    await db.query(`INSERT INTO users (username, password, first_name, last_name, email, is_admin)
+    VALUES ('testuser',
+            '$2b$12$AZH7virni5jlTTiGgEg4zu3lSvAw68qVEfSIOjJ3RqtbJbdW/Oi5q',
+            'Test',
+            'User',
+            'joel@joelburton.com',
+            FALSE),
+           ('testadmin',
+            '$2b$12$AZH7virni5jlTTiGgEg4zu3lSvAw68qVEfSIOjJ3RqtbJbdW/Oi5q',
+            'Test',
+            'Admin!',
+            'joel@joelburton.com',
+            TRUE);`);
+}
+
+seedIt(); 
